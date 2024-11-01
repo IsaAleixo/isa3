d7e21661e49062867bf9836c335da50cb77dd2ee
isa <147158501+IsaAleixo@users.noreply.github.com>
2024-10-31 22:36:35 -0300

    Add files via upload

--- isa3/app.js
+++ isa3/app.js
@@ -1,18 +1,27 @@
 function criaCartao(categoria, pergunta, respostas) {
-    let conteiner = document.getElementById('container')
-    let cartao = document.createElement('article')
-    cartao.className = 'cartao'
-  
-    cartao.innerHTML = `
-     <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
-    <div class="cartao__conteudo__pergunta">
-       <p>${pergunta}</p>
-    </div>
-    <div class="cartao__conteudo__resposta">
-      <p>${respostas}</p>
-    </div>
-  </div>`
-  
-      container.appendChild(cartao)
-    }
+  let conteiner = document.getElementById('container')
+  let cartao = document.createElement('article')
+  cartao.className = 'cartao'
+
+  cartao.innerHTML = `
+   <div class="cartao__conteudo">
+  <h3>${categoria}</h3>
+  <div class="cartao__conteudo__pergunta">
+     <p>${pergunta}</p>
+  </div>
+  <div class="cartao__conteudo__resposta">
+    <p>${respostas}</p>
+  </div>
+</div>`
+
+
+let respostaEstaVisivel = false
+
+function viraCartao(){
+  respostaEstaVisivel =!respostaEstaVisivel
+  cartao.classList.toggle('active', respostaEstaVisivel)
+
+   cartao.addEventListener('click', viraCartao)
+}
+    container.appendChild(cartao)
+  }
